947fab0d3ebd223804439b6c134e4b6e9c46f6f5
abdossamad <abdossamadhaghiri@gmail.com>
2024-05-15 20:17:01 +0330

    add caching for phase 1 services.

--- src/main/java/edu/sharif/nimbus/NimbusApplication.java
+++ src/main/java/edu/sharif/nimbus/NimbusApplication.java
@@ -2,8 +2,12 @@ package edu.sharif.nimbus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableCaching
+@EnableScheduling
 public class NimbusApplication {
 
     public static void main(String[] args) {
